a22780633c0ef5a969fd1395d50ed09868a18f7e
Piotray <159956456+Piotray@users.noreply.github.com>
2024-11-10 15:05:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Weather from "./weather";
+import App from "./App";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Weather />
+    <App />
   </StrictMode>,
-)
+);
